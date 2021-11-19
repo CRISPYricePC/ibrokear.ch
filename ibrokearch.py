39891ab0e9102dc57857705d08eb9ebd9974768d
Ben Mitchell <sc19b2m@leeds.ac.uk>
2021-11-19 14:02:23 +0000

    Uhhh

--- ibrokearch.py
+++ ibrokearch.py
@@ -1,7 +1,8 @@
 import requests
 import random
 import os
-from flask import Flask, redirect
+from io import BytesIO
+from flask import Flask, send_file
 from flask_caching import Cache
 
 API_URL = "https://g.tenor.com/v1"
@@ -9,25 +10,36 @@ API_KEY = os.getenv("TENOR_API_KEY", "not_provided")
 
 config = {
     "DEBUG": True,
-    "CACHE_TYPE": "SimpleCache"
+    "CACHE_TYPE": "FileSystemCache",
+    "CACHE_KEY_PREFIX": "SkillIssue",
+    "CACHE_DEFAULT_TIMEOUT": 86400
 }
+for env_var in ["CACHE_DIR"]:
+    config[env_var] = os.getenv(env_var)
+
 app = Flask(__name__)
 app.config.from_mapping(config)
 cache = Cache(app)
 
 
-@cache.memoize(86400)
+@cache.memoize()
 def get_tenor_gifs(search_term: str) -> list[str]:
     r = requests.get(
         f"{API_URL}/search?q={search_term}&key={API_KEY}&limit=50")
     results = r.json()["results"]
-    return [result["media"][0]["mediumgif"]["url"] for result in results]
+    return [result["media"][0]["gif"]["url"] for result in results]
+
+
+@cache.memoize()
+def pull_tenor_gif(url: str) -> BytesIO:
+    r = requests.get(url)
+    return BytesIO(r.content)
 
 
 @app.route("/")
 def skill_issue():
-    gifs = get_tenor_gifs("skill issue")
-    return redirect(random.choice(gifs))
+    file = pull_tenor_gif(random.choice(get_tenor_gifs("skill issue")))
+    return send_file(file, mimetype="image/gif")
 
 
 if (__name__ == "__main__"):
